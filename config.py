3fd580c68a545f39fcd8470ea1dc20f9b502ab52
Sagar Siwach <sagarsiwach19@outlook.com>
2024-03-24 08:32:31 +0530

    attempt

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
+import json
+
+def read_config():
     with open('config.json', mode='r', encoding='utf-8') as file:
         config = json.load(file)
         print("Config keys:", config.keys())  # Debug print
         return config
+
+def write_config(config):
+    try:
+        with open('config.json', mode='w', encoding='utf-8') as file:
+            json.dump(config, file, indent=4)
+            print("Config updated successfully")
+    except Exception as e:
+        print(f"Error writing to config.json: {e}")
