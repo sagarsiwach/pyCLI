388bf77da0c9e9c9ae8c4877b3c35cedaef1e787
Sagar Siwach <sagarsiwach19@outlook.com>
2024-03-25 07:25:12 +0530

    Attack Thread v2

--- attackThread.py
+++ attackThread.py
@@ -300,11 +300,13 @@ while True:
 
                 # Define train_troops_loop inside the main flow
                 def train_troops_loop():
-                    for _ in range(150):  # Train troops 150 times
+                    for _ in range(40):  # Train troops 150 times
                         train_troops()
 
                 # Use ThreadPoolExecutor to run train_troops_loop in two threads
                 with ThreadPoolExecutor(max_workers=4) as executor:
+                    executor.submit(train_troops_loop)
+                    executor.submit(train_troops_loop)
                     executor.submit(train_troops_loop)
                     executor.submit(train_troops_loop)
 
